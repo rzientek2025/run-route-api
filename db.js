7694ac563ae684f0eb3f2ddf0fae47abfdfafcaf
rzientek2025 <rzientek@gmail.com>
2025-10-20 21:59:59 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-// db.js - WERSJA WYŁĄCZONA (Nie robi nic, aby nie crashować aplikacji)
+// db.js - NIE używamy bazy danych.
 console.log("Moduł bazy danych ZAMKNIĘTY.");
 
 module.exports = {
-  // Zwraca funkcję-atrapy, aby reszta kodu (jeśli by istniała) się nie wywalała
   query: (text, params) => {
-    console.warn("UWAGA: Próba użycia bazy danych, ale jest ona wyłączona.");
+    console.warn("Baza danych jest wyłączona.");
     return Promise.resolve({ rows: [], rowCount: 0 }); 
   },
   pool: null
